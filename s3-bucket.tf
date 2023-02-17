3a399128212b19241da0f397cdbc2c31c1fafa6a
Hello World <you@example.com>
2023-02-17 06:53:11 +0000

    Added s3 bucket module 1

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,28 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
-}
-
-module "s3_bucket_for_logs" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket-for-logs"
-  acl    = "log-delivery-write"
-
-  # Allow deletion of non-empty bucket
-  force_destroy = true
-
-  attach_elb_log_delivery_policy = true
-}
-
-module "s3_bucket_for_logs" {
+module "s3_bucket_with_logs" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "my-s3-bucket-for-logs"
