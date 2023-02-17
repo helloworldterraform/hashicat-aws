7fa14c20ee2169a4379c7d40caaf667c3065033e
Hello World <you@example.com>
2023-02-17 07:16:38 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,6 @@
 module "s3_bucket" {
   source  = "app.terraform.io/helloworld123/s3-bucket/aws"
   version = "2.8.0"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+  
+  bucket_prefix = "hellworld"
 }
